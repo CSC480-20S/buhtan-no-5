7540adb69cfdc0f9ed1aec77557cf3c42b55f8e6
msglarson <anaaktge@oswego.edu>
2020-02-21 19:21:08 -0500

    Dont Run /simulate. Issues with Json and Bytes

--- Crypto/PublicTokenGenerator.py
+++ Crypto/PublicTokenGenerator.py
@@ -17,25 +17,34 @@ class TokenService():
             return server_key
         return secret_key
 
-    def verify_msg(self, client_resp):
-        user_id = client_resp['user_id']
-        user_type = client_resp["user_type"]
-        credits = client_resp["credits"]
-        user_hash = client_resp["hash"]
-        hashy=hl.sha256()
-        user_hex_vals = binascii.hexlify(str(str(user_id) + str(user_type) + str(credits)).encode("utf-8"))
-        hashy.update(user_hex_vals)
-        print(hashy.hexdigest())
-        print(user_hash)
-        if hashy.hexdigest() == user_hash:
-            return True
-        return False
+    def decrypt(self, client_resp):
+        box = secret.SecretBox(self.secret_key)
+        print(box.decrypt(client_resp['user_id']))
+        print(box.decrypt(client_resp["user_type"]))
+        print(box.decrypt(client_resp["credits"]))
+    # print(box.decrypt())
 
-    def create_keys(self):
-        # create the server key  and stores in a local file.
-        server_key = utils.random(secret.SecretBox.KEY_SIZE)
-        with open("Private.env", "wb") as f:
-            f.write(server_key)
-        return server_key
-    # updated token
-    # client_token={"user_id":0,"user_type":0,"credits":0,"hash":0}
+
+def verify_msg(self, client_resp):
+    user_id = client_resp['user_id']
+    user_type = client_resp["user_type"]
+    credits = client_resp["credits"]
+    user_hash = client_resp["hash"]
+    hashy = hl.sha256()
+    user_hex_vals = binascii.hexlify(str(str(user_id) + str(user_type) + str(credits)).encode("utf-8"))
+    hashy.update(user_hex_vals)
+    print(hashy.hexdigest())
+    print(user_hash)
+    if hashy.hexdigest() == user_hash:
+        return True
+    return False
+
+
+def create_keys(self):
+    # create the server key  and stores in a local file.
+    server_key = utils.random(secret.SecretBox.KEY_SIZE)
+    with open("Private.env", "wb") as f:
+        f.write(server_key)
+    return server_key
+# updated token
+# client_token={"user_id":0,"user_type":0,"credits":0,"hash":0}
--- test_server/TestServer.py
+++ test_server/TestServer.py
@@ -1,40 +1,63 @@
-import requests as req
-from flask import Flask, jsonify,json
-from flask_restful import Resource, Api
-from Crypto.TokenGenerator import UserToken
-import hashlib as hl
 import binascii
+import hashlib as hl
+import os
+import requests as req
+from nacl import secret, utils,encoding
+from dotenv import load_dotenv
+from flask import Flask
+from flask_restful import Resource, Api
 
 app = Flask(__name__)
 api = Api(app)
 
+
 class ClientServer(Resource):
+    dummy_user = {"user_id": 'kazookid', "user_type": 2, "credits": 20}
 
-    dummy_user= {"user_id":'kazookid',"user_type":2,"credits":20}
+    def __init__(self):
+        self.secret_key = self.load_secret_key()
+
+    def encrypt_fields(self):
+        print(self.secret_key)
+        box=secret.SecretBox(self.secret_key)
+        self.dummy_user["user_id"]=box.encrypt((self.dummy_user["user_id"]).encode('utf-8'))
+        self.dummy_user["user_type"]=box.encrypt(self.dummy_user["user_type"])
+        self.dummy_user["credits"]=box.encrypt(self.dummy_user["credits"])
+        print(type(self.dummy_user["credits"]))
 
 
+    def load_secret_key(self):
+        load_dotenv('secrets.env')
+        secret_key = os.getenv('secret_key')
+        if secret_key is None:
+            server_key = self.create_keys()
+            return server_key
+        return secret_key
+
+
+    def create_keys(self):
+    # create the server key  and stores in a local file.
+        server_key = utils.random(secret.SecretBox.KEY_SIZE)
+        with open("Private.env", "wb") as f:
+            f.write(server_key)
+        return server_key
 
     def get(self):
-        #to test the client getting the token
-        url="http://localhost:5000/tokenCreation"
+        # to test the client getting the token
+        url = "http://localhost:5000/tokenCreation"
         dummy_hash = hl.sha256()
-        dummy_hash.update(binascii.hexlify(str(self.dummy_user["user_id"]+str(self.dummy_user["user_type"])+str(self.dummy_user["credits"])).encode("utf-8")))
-        print(type(dummy_hash.hexdigest()))
-        self.dummy_user["hash"]=dummy_hash.hexdigest()
-        data=self.dummy_user
-        response=req.get(url,json=data)
+        self.encrypt_fields()
+        print(self.dummy_user["user_id"])
+        dummy_hash.update(binascii.hexlify(self.dummy_user["user_id"] + self.dummy_user["user_type"] + self.dummy_user["credits"]))
+        print(dummy_hash.hexdigest())
+        self.dummy_user["hash"] = dummy_hash.hexdigest()
+        data = self.dummy_user
+        response = req.get(url, json=data)
         return response.json()
-        # user_token=json.loads(response.text)
-        # print(type(user_token))
-        # decoded_token=UserToken().read_token(user_token)
-        #return jsonify(decoded_token)
 
 
-
-
-
-api.add_resource(ClientServer,'/simulate')
+api.add_resource(ClientServer, '/simulate')
 
 if __name__ == '__main__':
     # runs on localhosts
-    app.run(host='0.0.0.0', debug=True,port="6868")
+    app.run(host='0.0.0.0', debug=True, port="6868")
