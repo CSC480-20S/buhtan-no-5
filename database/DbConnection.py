fa52231ffab68c48b3321ef20142e2b05ef30169
nahyromolina48 <nmolina@oswego.edu>
2020-03-03 23:52:29 -0500

    Update DbConnection.py

--- database/DbConnection.py
+++ database/DbConnection.py
@@ -11,7 +11,7 @@ from dotenv import load_dotenv
 # curr -- Dict Object
 # new_data -- Dict Object
 
-
+# connects to the mongo atlas
 def connector():
     env_path = os.path.abspath(os.path.dirname(__file__))
     location = os.path.join(env_path, '.env')
@@ -19,25 +19,32 @@ def connector():
     client = MongoClient(os.getenv('MongoURL'))
     db = client["StudyStore"]
     return db
+# returns the whole database name "StudyStore"
 
 
+# deletes JSON in collection tb based on info
 def delete(tb, info):
     connect = connector()[tb]
     connect.delete_many(info)
+ # no return 
 
-
+# Updates a JSON that was already store in the database
+# curr is the JSON in database 
+# new_data is new JSON waiting to be stored in the database
 def edit(tb, curr, new_data):
     connect = connector()[tb]
     connect.update_many(curr, new_data)
+ # no return
 
-
+#Gets a JSON in collection tb in database based on info
 def get(tb, info):
     connect = connector()
     locate = connect[tb]
     check = locate.find(info)
     return check
+# return list of dicts based off info
 
-
+#Post info into the database in the collection tb
 def post(tb, info):
     connect = connector()
     locate = connect[tb]
@@ -51,6 +58,7 @@ def filtered(tb, info):
     filler = get(tb, info)
     search = filler.sort(info, 1)
     return search
+# returns list of dicts based off info that is sorted
 
 
 if __name__ == '__main__':
