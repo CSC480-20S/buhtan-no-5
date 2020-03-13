cfbd050a8fe8b673fd068e93eda5b7df5f3bddd3
Benjamin Groman <benp.groman@gmail.com>
2020-03-13 00:55:25 -0400

    Added keyword search, with options, and a limit to the actual search results.

--- endpoints/Search.py
+++ endpoints/Search.py
@@ -5,17 +5,28 @@ from studystore.FindingFiveStudyStoreUser import FindingFiveStudyStoreUser as f5
 from studystore.FindingFiveStudyStoreStudy import FindingFiveStudyStoreStudy as f5study
 import Purchase
 
+
 class Search(Resource):
     def get(self):
         """"Provides a list of studies from the database.
 
             Provides a list of studies, in JSON format, that meet some specified requirements.
-            If title is given, return all studies with that title.
+            A user id is required to verify the request is valid.
+            All other parameters are optional.
+            If title is given, return only studies with that title.
+            If keywords are given, return only studies with all of those keywords.
+            If keyword_all is false, each study need only have one or more keywords, not necessarily all of them.
+            If limit is given, no more than limit studies will be returned.
             ...in progress.
 
             Args:
                 user_id (String): The identifier for the user trying to search. Will be used for confirming access permission.
-                title: (String): The identifier for the study the user is trying to purchase.
+                title (String): The identifier for the study the user is trying to purchase.
+                keywords (String): Contains all the keywords that a study must have.
+                keyword_separator (String): Separates the keywords in the keywords parameter. Defaults to |.
+                keyword_all (Boolean): If false, any non-empty subset of the keywords is sufficient to match.
+                limit (Integer): The maximum number of studies to return. Defaults to unlimited when missing or negative.
+
 
             Returns:
                 JSON: The list of studies that meet the specified requirements.
@@ -24,21 +35,40 @@ class Search(Resource):
         parser = reqparse.RequestParser(bundle_errors=True)
         parser.add_argument("user_id", type=str)
         parser.add_argument("title", type=str)
+        parser.add_argument("keywords", type=str)
+        parser.add_argument("keyword_separator", type=str)
+        parser.add_argument("keyword_all", type=bool)
+        parser.add_argument("limit", type=int)
         returned_args = parser.parse_args()
         user_id = returned_args.get("user_id", None)
         title = returned_args.get("title", None)
-        # verify the parameters exist
-        # will need to change this to allow title=None when other specifying parameters are given
-        if user_id == None or title == None:
-            return jsonify({"error": "missing parameter"})
+        keywords_unsplit = returned_args.get("keywords", None)
+        keyword_separator = returned_args.get("keyword_separator", "|")
+        keyword_all = returned_args.get("keyword_all", True)
+        limit = returned_args.get("limit", -1)
+        # verify the required parameters exist
+        if user_id == None:
+            return jsonify({"error": "missing user_id parameter"})
         # get the necessary data from the database
         # this exists for verifying we have an authenticated user
         user = Purchase.getUser(user_id)
+        #should make some check that the user's session is still valid
+        #i.e. last authentication within 30 minutes
 
         # build search parameters
-        params = { "Title":title }
+        params = {}
+        if title != None:
+            params["Title"] = title
+        if keywords_unsplit != None:
+            keywords = keywords_unsplit.split(keyword_separator)
+            #intersection/and
+            if keyword_all == True:
+                params["Keywords"] = {"$all": keywords}
+            #union/or
+            else:
+                params["Keywords"] = { "$in": keywords}
         # query database
-        studyList = getStudies(params)
+        studyList = getStudies(params, limit)
         # convert output
         out = {}
         for i in range(len(studyList)):
