8ffe615dbb54acc926baf47c352bea0bc4dcbf3a
msglarson <anaaktge@oswego.edu>
2020-03-15 14:45:51 -0400

    Removed update script from /

--- deployment/updater.sh
+++ deployment/updater.sh
@@ -2,6 +2,5 @@
 git checkout master
 git fetch
 git pull --rebase origin master
-cp main.py testy.py
 #to-do
 #updating the documentation to reflect the changes from master
--- updater.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-git checkout master
-git fetch
-git pull --rebase origin master
-cp main.py testy.py
-#to-do
-#updating the documentation to reflect the changes from master
