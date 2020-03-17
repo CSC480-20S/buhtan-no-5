9a37d388524170aa4f136f72e3790cdb3103e86a
msglarson <anaaktge@oswego.edu>
2020-03-17 16:06:02 -0400

    Fixed the recursion

--- deployment/updater.sh
+++ deployment/updater.sh
@@ -7,5 +7,6 @@ git pull --rebase origin master
 kill $(pgrep -u $USER python)
 
 cd ../ && nohup python3 launcher.py --bind 129.3.20.26:12100 launcher:app & >/dev/null 2>1&
-cd ~/csc480/deployment && nohup python3 poll_github.py &>/dev/null 2 >1&
+cd ~/csc480/deployment && nohup python3 poll_github.py &>/dev/null 2>1&
 kill $(pgrep -u $USER updater.sh)
+#this solution will not update at the same time, will be n seconds from time of execution.
