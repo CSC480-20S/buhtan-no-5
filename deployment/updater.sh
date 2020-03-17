47a7946a9e046bc2610ea244d27274472ae3bbaa
msglarson <anaaktge@oswego.edu>
2020-03-17 15:31:31 -0400

    Imroved Deployment Script

--- deployment/updater.sh
+++ deployment/updater.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+
+#this scripts get the most recent from man and relaunches the server
 git checkout master
 git fetch
 git pull --rebase origin master
+kill $(pgrep -u $USER python)
+
+cd ../ && nohup python3 launcher.py --bind 129.3.20.26:12100 launcher:app & >/dev/null &
+cd deployment && nohup python3 poll_github.py &>/dev/null &
 #to-do
 #updating the documentation to reflect the changes from master
