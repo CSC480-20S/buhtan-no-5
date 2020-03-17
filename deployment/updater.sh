800e176b03a888f36ce4e8f0295332693678be46
msglarson <anaaktge@oswego.edu>
2020-03-17 15:38:49 -0400

    Debugging

--- deployment/updater.sh
+++ deployment/updater.sh
@@ -4,9 +4,9 @@
 git checkout master
 git fetch
 git pull --rebase origin master
-kill $(pgrep -u $USER python)
+echo kill $(pgrep -u $USER python)
 
 cd ../ && nohup python3 launcher.py --bind 129.3.20.26:12100 launcher:app & >/dev/null &
-cd deployment && nohup python3 poll_github.py &>/dev/null &
+cd ~/csc480/deployment && nohup python3 poll_github.py &>/dev/null &
 #to-do
 #updating the documentation to reflect the changes from master
