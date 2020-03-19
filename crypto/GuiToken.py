72194b0c37c0c3a315953635e545c71d36e3d003
msglarson <anaaktge@oswego.edu>
2020-03-19 15:10:29 -0400

    Added endpoint to simulate F5 user token

--- crypto/GuiToken.py
+++ crypto/GuiToken.py
@@ -1,16 +1,30 @@
 import jwt
 import crypto.PublicTokenGenerator as pg
 import datetime
-from flask_restful import Resource, reqparse
+from flask import jsonify
+from flask_restful import Resource, reqparse,abort
 
 
-class Generator():
+class Generator(Resource):
     def __init__(self):
         self.key = pg.TokenService.load_secret_key(pg)
+
+    def get(self):
+        '''
+        This will create a jwt token which is valid for 60 minutes.
+        Returns:
+
+        '''
+        parser = reqparse.RequestParser(bundle_errors=True)
+        parser.add_argument("user_id", type=str, required=True, help="The user ID of the owner is a String.")
+        returned_args = parser.parse_args()
+        token=self.generate_token(returned_args.get('user_id',None))
+        return jsonify({'token':token.decode('UTF-8')})
 
     def generate_token(self, user_id):
         '''
         Generates the jwt token for access to endpoints.
+        This is to simulate F5 providing us with a user token.
 
         Args:
             user_id(Str): Id of the user requesting a token
@@ -18,7 +32,8 @@ class Generator():
         Returns:
             token(dict): Dict representing the token
         '''
-
+        if user_id is None:
+              abort(401, description=['UserId must be provided'])
         try:
             utc = datetime.datetime.utcnow()
 
@@ -44,8 +59,8 @@ class Generator():
             payload = jwt.decode(alleged_token, self.key)
             return payload['sub']
         except jwt.InvalidTokenError as e:
-            return {'msg':"Invalid token.Please try again.",'err':e}
+            return {'msg': "Invalid token.Please try again.", 'err': e}
         except jwt.DecodeError as e:
-            return {'msg':"Error decoding the token. Please try again.",'err':e}
+            return {'msg': "Error decoding the token. Please try again.", 'err': e}
         except jwt.exceptions.InvalidSignatureError as e:
-            return {'msg':"Expired Signature, Please try again",'err':e}
+            return {'msg': "Expired Signature, Please try again", 'err': e}
--- main.py
+++ main.py
@@ -3,10 +3,13 @@ from flask_cors import CORS
 from flask_restful import Resource, Api, reqparse
 from crypto.PublicTokenGenerator import TokenService
 from endpoints import transaction_blueprint as tbp
+from crypto import crypto_blueprint as cbp
 from exceptions import Authentication as auth
+
 app = Flask(__name__)
 app.register_blueprint(tbp.trans_bp)
-app.register_error_handler(401,auth.handle_auth)
+app.register_blueprint(cbp.crypto_bp)
+app.register_error_handler(401, auth.handle_auth)
 CORS(app)
 api = Api(app)
 
