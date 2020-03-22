c527101a67744b8ca6e58219a2dfe1b0101ac36d
Benjamin Groman <benp.groman@gmail.com>
2020-03-22 14:59:38 -0400

    Added price parameters.

--- endpoints/Search.py
+++ endpoints/Search.py
@@ -12,12 +12,17 @@ class Search(Resource):
         """"Provides a list of studies from the database.
 
             Provides a list of studies, in JSON format, that meet some specified requirements.
-            A user id is required to verify the request is valid.
-            All other parameters are optional.
+            All parameters are optional, except for the token required for authentication.
+            Providing none of the optional parameters will result in all studies being returned,
+            but may result in a connection time out.
             If title is given, return only studies with that title.
             If keywords are given, return only studies with all of those keywords.
             If keyword_all is false, each study need only have one or more keywords, not necessarily all of them.
             If limit is given, no more than limit studies will be returned.
+            If price_min is given, return only studies at that price or higher.
+            If price_max is given, return only studies at that price or lower.
+            If price_min is greater than price_max, ignore price_max and negative values of price_min.
+            For the purposes of this method, the price of a study may be negative.
             ...in progress.
 
             Args:
@@ -26,6 +31,8 @@ class Search(Resource):
                 keyword_separator (String): Separates the keywords in the keywords parameter. Defaults to |.
                 keyword_all (Boolean): If false, any non-empty subset of the keywords is sufficient to match.
                 limit (Integer): The maximum number of studies to return. Defaults to unlimited when missing or negative.
+                price_min (Integer): The minimum price, in credits, that a study may have.
+                price_max (Integer): The maximum price, in credits, that a study may have.
 
 
             Returns:
@@ -38,6 +45,8 @@ class Search(Resource):
         parser.add_argument("keyword_separator", type=str, default="|")
         parser.add_argument("keyword_all", type=bool, default=True)
         parser.add_argument("limit", type=int, default=-1)
+        parser.add_argument("price_min", type=int, default=0)
+        parser.add_argument("price_max", type=int, default=-1)
 
         # the second parameter to each method call is purely for consistency,
         # they don't actually do anything. They should match the defaults above.
@@ -47,6 +56,8 @@ class Search(Resource):
         keyword_separator = returned_args.get("keyword_separator", "|")
         keyword_all = returned_args.get("keyword_all", True)
         limit = returned_args.get("limit", -1)
+        price_min = returned_args.get("price_min", 0)
+        price_max = returned_args.get("price_max", -1)
 
         # build search parameters
         params = {}
@@ -60,6 +71,16 @@ class Search(Resource):
             # union/or
             else:
                 params["Keywords"] = {"$in": keywords}
+        if price_min <= 0 and price_max < price_min:
+            pass
+        elif price_min > 0 and price_max < price_min:
+            params["CostinCredits"] = {"$gte": price_min}
+        elif price_min == price_max:
+            params["CostinCredits"] = price_min
+        else:
+            # price_max is greater than price_min
+            # using implicit $and operation
+            params["CostinCredits"] = {"$gte": price_min, "$lte": price_max}
         # query database
         studyList = Auxiliary.getStudies(params, limit)
         # convert output
