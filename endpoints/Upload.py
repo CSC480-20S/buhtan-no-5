225b793b77d48c5382a6bc100ccf2c1e4012bc3f
Benjamin Groman <benp.groman@gmail.com>
2020-03-31 23:45:54 -0400

    Added images, abstract, and author_id parameters, fixed boolean parameter type, cleaned out some unneeded lines, and switched to posting a database-friendly document.

--- endpoints/Upload.py
+++ endpoints/Upload.py
@@ -1,5 +1,5 @@
 from flask import jsonify
-from flask_restful import Resource, reqparse
+from flask_restful import Resource, reqparse, inputs
 from endpoints import Auxiliary
 from database import DbConnection
 from studystore.FindingFiveStudyStoreStudy import FindingFiveStudyStoreStudy
@@ -11,11 +11,8 @@ class Upload(Resource):
         # obtain parameters
         parser = reqparse.RequestParser(bundle_errors=True)
         parser.add_argument("title", type=str, required=True, help="Title should be string.")
-
         parser.add_argument("author", type=str, required=True,
                             help="Issue author should be String")
-        # we currently have no such thing as a username just an ID, so this is necessarily an ID if its to do anything
-
         parser.add_argument("costInCredits", type=int, required=True, help="Issue with cost . should be int")
         parser.add_argument("purpose", type=str, required=True, help="missing study's purpose statement, string")
         parser.add_argument("references", type=str, action='append', required=True, help="missing references used to"
@@ -32,15 +29,15 @@ class Upload(Resource):
                                                                            "number of responses expected from user, int")
         parser.add_argument("num_trials", type=int, required=True,
                             help="Issue with the number of trials within a study, int")
-        parser.add_argument("randomize", type=bool, required=True, help="Issue with randomized(question order) "
+        parser.add_argument("randomize", type=inputs.boolean, required=True, help="Issue with randomized(question order) "
                                                                         "param,bool")
         parser.add_argument("duration", type=int, required=True, help="Issue with  expected run time of the study "
                                                                       "from perspective of the surveyed individual, int")
-        parser.add_argument("rating", type=int, help="Issue with rating, int")  # not required presumably
-        # todo check this is in fact required v
         parser.add_argument("institution", type=str, required=True, help="Issue with institution, string")
-        # todo template
         parser.add_argument("template", type=str, required=True, help="Issue with / Missing template, string")
+        parser.add_argument("images", type=str, action="append", required=True, help="Images are stored/referenced with Strings.")
+        parser.add_argument("abstract", type=str, required=True, help="Abstract is a description String.")
+        parser.add_argument("author_id", type=str, required=True, help="The author_id is the user_id of the uploading user.")
 
         returned_args = parser.parse_args()
 
@@ -49,7 +46,6 @@ class Upload(Resource):
         study_id = counter["Next_ID"]
         title = returned_args.get("title", None)
         author = returned_args.get("author", None)
-
         costInCredits = returned_args.get("costInCredits", None)
         purpose = returned_args.get("purpose", None)
 
@@ -62,20 +58,18 @@ class Upload(Resource):
         num_responses = returned_args.get("num_responses", None)
         num_trials = returned_args.get("num_trials", None)
         randomize = returned_args.get("randomize", None)
-        print(randomize)
-
         duration = returned_args.get("duration", None)
-        print(duration)
         rating = returned_args.get("rating", None)
-        print(rating)
         institution = returned_args.get("institution", None)
-        # todo : template- not sure how to do this at all
         template = returned_args.get("template", None)
+        images = returned_args.get("images", None)
+        abstract = returned_args.get("abstract", None)
+        author_id = returned_args.get("author_id", None)
         study = FindingFiveStudyStoreStudy(study_id, title, author, costInCredits, purpose, references, categories,
                                            subcategories, keywords, num_stimuli, num_responses, randomize,
-                                           duration, num_trials, rating, institution, template)
+                                           duration, num_trials, rating, institution, template, images, abstract, author_id)
         # connect = DbConnection.connector()["Studies"]
-        study_dict = FindingFiveStudyStoreStudy.build_dict(study)
+        study_dict = study.build_database_doc()
         # connect.insert_one("Studies", study_dict).inserted_id
 
         DbConnection.connector()["Studies"].insert(study_dict)
