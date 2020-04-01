7cd8581f4ba54478606e9c119c6053c3bc3308c3
msglarson <anaaktge@oswego.edu>
2020-04-01 19:40:50 -0400

    Started to Rank Words Based off search

--- database/cache.py
+++ database/cache.py
@@ -6,12 +6,13 @@ from gui_endpoints.preview_study import x
 class SearchCache():
     def __init__(self):
         self.r = redis.Redis(host='localhost', port=6379, db=0)
+        self.max_set_size=400
 
     def check_existence(self, title):
-        result = self.r.exists("seach:" + title)
+        result = self.r.exists(title)
         if result is 1:
-            return True, self.r.hgetall("seach:" + title)
-        return False, None
+            return True
+        return False
 
     def add_serach_query(self, response, query):
         key = "seach:" + str(query)
@@ -47,13 +48,28 @@ class SearchCache():
     def search_one_word(self, input):
         sorted_set_id = "tmp:" + str(uuid.uuid5(uuid.NAMESPACE_OID, input))
         print(sorted_set_id)
-        for id in self.r.zrange(sorted_set_id, 0, -1):
-            yield self.r.hget(id, 'title')
+        for id in self.r.zrange(sorted_set_id, 0, 5):
+                print(id)
+                yield self.r.hget(id, 'title')
+
+    def update_score(self,selected_word,prefix):
+        sorted_set_id=self.get_search_id(prefix)
+        set_size=self.r.scard(sorted_set_id)
+        if not self.check_existence(sorted_set_id):
+            if set_size<self.max_set_size:
+                self.r.zadd(sorted_set_id,{selected_word:1})
+            else:
+                last_ele,score=self.r.zrange(sorted_set_id,-1,-1,withscores=True)
+                self.r.srem(sorted_set_id,last_ele)
+                self.r.zadd(sorted_set_id,{selected_word:score+1})
+        self.r.zincrby(sorted_set_id,1,selected_word)
+
 
     def get_search_id(self, word):
         return "tmp:" + str(uuid.uuid5(uuid.NAMESPACE_OID, word))
 
     def search_multiple_word(self, input):
+        ''' Does not work as of 04012020'''
         words = input.split(" ")
         print(self.get_search_id(input))
         print(list(map(self.get_search_id, words)))
@@ -86,9 +102,8 @@ s = SearchCache()
 s.add_new_word("austin")
 print(str(uuid.uuid5(uuid.NAMESPACE_OID, '')))
 # s.create_basic_prefix()
-for word in s.search_multiple_word("app zuc"):
+for word in s.search_one_word("za"):
     print(word)
-    print("one")
 # res=s.add_serach_query(x.build_dict(),"hell0 world")
 # status,study = s.check_existence("hell0 world")
 # print(status,study)
