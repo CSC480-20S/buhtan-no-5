703463701785c6a531826cbcd8cebf02715ac4c1
msglarson <47165374+msglarson@users.noreply.github.com>
2020-04-03 08:16:09 -0400

    Cache (#42)
    * Added Exception Log Folder
    
    * Added aborts

--- crypto/GuiToken.py
+++ crypto/GuiToken.py
@@ -2,7 +2,7 @@ import jwt
 from crypto.PublicTokenGenerator import TokenService as  pg
 import datetime
 from flask import jsonify
-from flask_restful import Resource, reqparse,abort
+from flask_restful import Resource, reqparse, abort
 
 
 class Generator(Resource):
@@ -19,8 +19,8 @@ class Generator(Resource):
         parser = reqparse.RequestParser(bundle_errors=True)
         parser.add_argument("user_id", type=str, required=True, help="The user ID of the owner is a String.")
         returned_args = parser.parse_args()
-        token=self.generate_token(returned_args.get('user_id',None))
-        return jsonify({'token':token.decode('UTF-8')})
+        token = self.generate_token(returned_args.get('user_id', None))
+        return jsonify({'token': token.decode('UTF-8')})
 
     def generate_token(self, user_id):
         '''
@@ -34,12 +34,12 @@ class Generator(Resource):
             token(dict): Dict representing the token
         '''
         if user_id is None:
-              abort(401, description=['UserId must be provided'])
+            abort(401, description=['UserId must be provided'])
         try:
             utc = datetime.datetime.utcnow()
 
             resp = {'iat': utc,
-                    'exp': (utc + datetime.timedelta(days=50,minutes=60)),
+                    'exp': (utc + datetime.timedelta(days=50, minutes=60)),
                     'sub': user_id,
                     }
 
@@ -60,8 +60,8 @@ class Generator(Resource):
             payload = jwt.decode(alleged_token, self.key)
             return payload['sub']
         except jwt.InvalidTokenError as e:
-             abort(401,message=e)
+            abort(401, message=e)
         except jwt.DecodeError as e:
-            return (401,message=e)
+            abort(401, message=e)
         except jwt.exceptions.InvalidSignatureError as e:
-            return (401,message=e)
+            abort(401, message=e)
