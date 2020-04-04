1d8a0d41d718c49160f19393593169985b1a99bb
Benjamin Groman <benp.groman@gmail.com>
2020-04-04 15:34:37 -0400

    Added authorList parameter/attribute to FFSSUser.

--- studystore/FindingFiveStudyStoreUser.py
+++ studystore/FindingFiveStudyStoreUser.py
@@ -1,10 +1,11 @@
 class FindingFiveStudyStoreUser:
-    def __init__(self, userID, numCredits, ownedStudies, viewedStudies, wishList):
+    def __init__(self, userID, numCredits, ownedStudies, viewedStudies, wishList, authorList):
         self.userID = userID
         self.numCredits = numCredits
         self.ownedStudies = ownedStudies  # list of FFSS studies
         self.viewedStudies = viewedStudies  # list of FFSS studies
         self.wishList = wishList  # wish list of FFSS studies
+        self.authorList = authorList
 
     def get_userId(self):
         return self.userID
@@ -21,6 +22,9 @@ class FindingFiveStudyStoreUser:
     def get_wishList(self):
         return self.wishList
 
+    def get_authorList(self):
+        return self.authorList
+
     def set_userId(self, newUserID):
         self.userID = newUserID
 
@@ -35,3 +39,6 @@ class FindingFiveStudyStoreUser:
 
     def set_viewedStudies(self, newViewedStudies):
         self.viewedStudies = newViewedStudies
+
+    def set_authorList(self, newAuthorList):
+        self.authorList = newAuthorList
