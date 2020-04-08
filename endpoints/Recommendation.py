8cda452afb9af92c2aebb998bf185f7089255c68
ethan <emesser@oswego.edu>
2020-04-08 00:55:49 -0400

    small refactor on recommendations
    made the code cleaner, handled some edge cases about recommending studies authored by user, etc

--- endpoints/Recommendation.py
+++ endpoints/Recommendation.py
@@ -11,7 +11,6 @@ class Recommendation(Resource):
 
     @Auxiliary.auth_dec
     def get(self, **kwargs):
-        # obtain parameters
         parser = reqparse.RequestParser(bundle_errors=True)
         parser.add_argument("user_id", type=str, required=True,
                             help="The user_id is string representation of user's ID.")
@@ -21,72 +20,34 @@ class Recommendation(Resource):
         pref_study_ids = set(
             user.get_ownedStudies() + user.get_viewedStudies() + user.get_wishList() + user.get_authorList())
         studies = [study for id in pref_study_ids if (study := Auxiliary.getOptionalStudy(id))]
-        # categories = [cat for study in studies if(cat := study.get_categories())]
-        # subcategories = [sub for study in studies if()]
-        categorical_key_terms = []
-
-        functions = [Study.get_categories, Study.get_subcategories, Study.get_keywords]
-        cats, subcats, keywordz = [flatten(x for study in studies if (x := remove_nones(f(study)))) for f in functions]
-        # print(cats)
-        # print(subcats)
-        # print(keywordz)
-
-        duplicate_cats, duplicate_subcats, duplicate_keywordz = \
-            [[term for term, count in Counter(c).items() if count > 1]
-             for c in [cats, subcats, keywordz]]
-
+        if not studies:
+            return {"error": "you don't have any study interactions, use our site to GET recommendations!"}
         params = {}
-        if duplicate_cats:
-            params["Categories"] = {"$in": duplicate_cats}
-        elif cats:
-            params["Categories"] = {"$in": cats}
 
-        if duplicate_subcats:
-            params["Sub_Categories"] = {"$in": duplicate_subcats}
-        elif subcats:
-            params["Sub_Categories"] = {"$in": subcats}
+        def valid_study(study):
+            return study.get_authorID() != user_id
 
-        if duplicate_keywordz:
-            params["Keywords"] = {"$in": duplicate_keywordz}
-        elif keywordz:
-            params["Keywords"] = {"$in": keywordz}
+        def add_param(function, param_name):
+            values = flatten(x for study in studies if (x := remove_nones(function(study))))
+            duplicates = [e for e, count in Counter(values).items() if count > 1]
 
-        print(list(set(Auxiliary.getStudies(params, 75))))
+            if duplicates:
+                params[param_name] = {"$in": duplicates}
+            elif values:
+                params[param_name] = {"$in": values}
 
-        studylist = list(set(Auxiliary.getStudies(params, 75)))
+        add_param(Study.get_categories, "Categories")
+        add_param(Study.get_subcategories, "Sub_Categories")
+        add_param(Study.get_keywords, "Keywords")
 
-        studylist.sort(key=lambda study: study.get_rating(), reverse=True)
+        full_study_list = list(set(Auxiliary.getStudies(params, 75)))
+        filtered_study_list = list(filter(valid_study, full_study_list))
 
-        return jsonify(Auxiliary.studyListToDictList(studylist))
+        study_list = filtered_study_list if filtered_study_list else full_study_list
+        study_list.sort(key=lambda study: study.get_rating(), reverse=True)
 
-        print([x["rating"] for x in Auxiliary.studyListToDictList(studylist)])
-        # print(len(studylist))
+        return jsonify(Auxiliary.studyListToDictList(study_list))
 
-
-
-        '''
-        cats = []
-        subcats = []
-        keywordz = []
-
-        for study in studies:
-            if (temp := study.get_categories()) and (temp2 := study.get_subcategories()) and (
-                    temp3 := study.get_keywords()):
-                cats += temp
-                subcats += temp2
-                keywordz += temp3
-                
-        '''
-
-        # print(categorical_key_terms)
-        # print(list(dict.fromkeys(categorical_key_terms)))
-        # print([term for term, count in collections.Counter(categorical_key_terms).items() if count > 1]) # repeated categorical keywords
-        # print(cats)
-        # print(subcats)
-
-        # repeated_cats = [term for term, count in collections.Counter(keywordz).items() if count > 1]
-        # repeated_subcats = [term for term, count in collections.Counter(subcats).items() if count > 1]
-        # repeated_keywords = [term for term, count in collections.Counter(keywordz).items() if count > 1]
 
 
 def flatten(list):
