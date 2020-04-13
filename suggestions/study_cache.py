36ac380fe6602abcbe1701f8ee5bc53a86e2ad76
msglarson <anaaktge@oswego.edu>
2020-04-12 20:06:13 -0400

    Added function to get study from cache

--- suggestions/study_cache.py
+++ suggestions/study_cache.py
@@ -1,7 +1,16 @@
 import redis
-
+import os
 class StudyCache():
-    ''''''
-    def __init__(self):
-        self.r = redis.Redis(host='localhost', port=6379, db=0)
-        self.max_set_size = 400
+    '''A Redis Cache to store FindingFiveStudyStoreStudy Objects.'''
+    def __init__(self): #6378
+        self.r = redis.Redis(host='localhost', port=6378, db=0)
+
+    def get_study_from_cache(self, title):
+        result = self.r.exists(title)
+        if result == 1:
+            return self.r.hgetall(title)
+        return False
+
+if __name__ == "__main__":
+    s=StudyCache()
+    s.get_study_from_cache("example")
