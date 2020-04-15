998cbdaf646d7ae77401de0fc1c7e29db884519a
Benjamin Groman <benp.groman@gmail.com>
2020-04-15 13:47:23 -0400

    Removed /studyPreview from the blueprint.

--- endpoints/transaction_blueprint.py
+++ endpoints/transaction_blueprint.py
@@ -3,7 +3,6 @@ from flask_restful import Api
 from endpoints.Deliver import Deliver
 from endpoints.Purchase import Purchase
 from database import studies
-from gui_endpoints import preview_study
 from endpoints.Search import Search
 from endpoints.IsOwned import IsOwned
 from endpoints.GetOwned import GetOwned
@@ -30,7 +29,6 @@ api.add_resource(Deliver, '/deliver')
 api.add_resource(Purchase, '/purchase')
 api.add_resource(studies.EndPointOwnedStudies, '/owned')
 api.add_resource(studies.EndPointViewedStudies, '/previewed')
-api.add_resource(preview_study.EndPoint_PreviewStudies, '/studyPreview')
 api.add_resource(Search, '/search')
 api.add_resource(IsOwned, '/isOwned')
 api.add_resource(GetOwned, '/getOwned')
