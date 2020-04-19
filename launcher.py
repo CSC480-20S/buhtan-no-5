a42ecab5a0b456a1797e2291c615f849f85eab1e
msglarson <anaaktge@oswego.edu>
2020-04-19 15:09:11 -0400

    Working on Applying App Factory Pattern

--- launcher.py
+++ launcher.py
@@ -1,6 +1,10 @@
 import gunicorn.app.wsgiapp as wsgi
+from application import create_app
 from main import app
+
+app = create_app()
 
 if __name__ == '__main__':
     # runs on localhosts
-    wsgi.run()
+    app.run(host='0.0.0.0',port=12100, debug=True)
+    #wsgi.run()
